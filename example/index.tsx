2b7dab2cddff59103ccf96cc53772348d551ff7a
Jon Stevens <latchkey@gmail.com>
2019-10-21 09:16:00 +0700

    fix the grid into two columns

--- example/index.tsx
+++ example/index.tsx
@@ -1,31 +1,15 @@
-import 'react-app-polyfill/ie11';
-
-import {
-	AppBar,
-	Grid,
-	Link,
-	Paper,
-	Toolbar,
-	Typography,
-} from '@material-ui/core';
+import { AppBar, Grid, Link, Paper, Toolbar, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import React from 'react';
+import 'react-app-polyfill/ie11';
 import ReactDOM from 'react-dom';
 
 import { Form } from 'react-final-form';
 
 import * as Yup from 'yup';
 
-import {
-	CheckboxData,
-	Checkboxes,
-	DatePicker,
-	makeValidate,
-	Select,
-	SelectData,
-	TextField,
-} from '../src';
+import { CheckboxData, Checkboxes, DatePicker, makeValidate, Select, SelectData, TextField } from '../src';
 import { RadioData, Radios } from '../src/Radios';
 
 interface FormData {
@@ -46,7 +30,7 @@ const useStyles = makeStyles((theme: Theme) =>
 			top: 'auto',
 			bottom: 0,
 		},
-	})
+	}),
 );
 
 const validateSchema = makeValidate(
@@ -56,7 +40,7 @@ const validateSchema = makeValidate(
 		hello: Yup.string().required(),
 		cities: Yup.string().required(),
 		gender: Yup.string().required(),
-	})
+	}),
 );
 
 const App = () => {
@@ -107,52 +91,45 @@ const App = () => {
 					render={({ handleSubmit, values, errors }) => (
 						<form onSubmit={handleSubmit} noValidate>
 							<Grid container>
-								<Grid item xs>
-									<Checkboxes
-										label="Check at least one..."
-										required={true}
-										name="best"
-										data={checkboxData}
-										error={errors.best}
-									/>
+								<Grid item xs={6}>
+									<Grid item>
+										<Checkboxes
+											label="Check at least one..."
+											required={true}
+											name="best"
+											data={checkboxData}
+											error={errors.best}
+										/>
+									</Grid>
+									<Grid item>
+										<Radios
+											label="Pick a gender"
+											required={true}
+											name="gender"
+											data={radioData}
+											error={errors.gender}
+										/>
+									</Grid>
+									<Grid item>
+										<DatePicker label="Pick a date" name="date" required={true}/>
+									</Grid>
+									<Grid item>
+										<TextField label="Hello world" name="hello" required={true}/>
+									</Grid>
+									<Grid item>
+										<Select
+											label="Pick a city..."
+											name="cities"
+											error={errors.cities}
+											required={true}
+											data={selectData}
+										/>
+									</Grid>
 								</Grid>
-								<Grid item xs>
+								<Grid item xs={6}>
 									<Typography>Form field data</Typography>
 									<pre>{JSON.stringify(values, undefined, 2)}</pre>
 								</Grid>
-							</Grid>
-							<Grid item xs>
-								<Radios
-									label="Pick a gender"
-									required={true}
-									name="gender"
-									data={radioData}
-									error={errors.gender}
-								/>
-							</Grid>
-							<Grid container>
-								<Grid item xs>
-									<DatePicker label="Pick a date" name="date" required={true} />
-								</Grid>
-								<Grid item xs></Grid>
-							</Grid>
-							<Grid container>
-								<Grid item xs>
-									<TextField label="Hello world" name="hello" required={true} />
-								</Grid>
-								<Grid item xs></Grid>
-							</Grid>
-							<Grid container>
-								<Grid item xs>
-									<Select
-										label="Pick a city..."
-										name="cities"
-										error={errors.cities}
-										required={true}
-										data={selectData}
-									/>
-								</Grid>
-								<Grid item xs></Grid>
 							</Grid>
 						</form>
 					)}
@@ -189,4 +166,4 @@ const App = () => {
 	);
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
