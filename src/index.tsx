38d8e6988b489d083a1f1c8dba081db96059da4b
Julien Goux <julien.goux@live.fr>
2020-01-28 15:07:17 +0100

    export SelectProps

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 export { Autocomplete, AutocompleteData } from './Autocomplete';
 export { Checkboxes, CheckboxData } from './Checkboxes';
 export { Radios, RadioData } from './Radios';
-export { Select, SelectData } from './Select';
+export { Select, SelectData, SelectProps } from './Select';
 export { KeyboardDatePicker } from './KeyboardDatePicker';
 export { DatePicker } from './DatePicker';
 export { TimePicker } from './TimePicker';
