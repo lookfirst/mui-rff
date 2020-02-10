914813f1dca669669af5a1f5d4a7438a05ecc7e4
Ya Zhuang <zhuangya@users.noreply.github.com>
2020-02-10 14:39:25 +0800

    adapt more input type for TextField
    input with `search` type does not hold a role of 'textbox', so the test
    would fail for `search` type, so the `search` type is not included yet.
    
    ref #116

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -9,6 +9,12 @@ export const TYPE_TEXT = 'text';
 export const TYPE_EMAIL = 'email';
 export const TYPE_NUMBER = 'number';
 export const TYPE_URL = 'url';
+export const TYPE_TELEPHONE = 'tel';
+export const TYPE_DATE = 'date';
+export const TYPE_DATETIME_LOCAL = 'datetime-local';
+export const TYPE_MONTH = 'month';
+export const TYPE_TIME = 'time';
+export const TYPE_WEEK = 'week';
 
 // Restricts the type values to 'password', 'text', 'email', 'number', and 'url'.
 export type TEXT_FIELD_TYPE =
@@ -16,7 +22,13 @@ export type TEXT_FIELD_TYPE =
 	| typeof TYPE_TEXT
 	| typeof TYPE_EMAIL
 	| typeof TYPE_NUMBER
-	| typeof TYPE_URL;
+	| typeof TYPE_URL
+	| typeof TYPE_TELEPHONE
+	| typeof TYPE_DATE
+	| typeof TYPE_DATETIME_LOCAL
+	| typeof TYPE_MONTH
+	| typeof TYPE_TIME
+	| typeof TYPE_WEEK;
 
 export type TextFieldProps = Omit<MuiTextFieldProps, 'type'> & {
 	type: TEXT_FIELD_TYPE;
