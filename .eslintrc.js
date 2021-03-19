29430d41114556fca3bce57ca28a0ce82e96c9ea
Jon Stevens <latchkey@gmail.com>
2021-03-18 19:50:47 -0700

    latest deps, husky changes, remove prettier for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-	plugins: ['autofix'],
+	plugins: ['autofix', 'react', 'react-hooks', 'unused-imports'],
 	parserOptions: {
 		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
 		sourceType: 'module', // Allows for the use of imports
@@ -17,16 +17,15 @@ module.exports = {
 		'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react,
 		'plugin:react-hooks/recommended',
 		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 	],
 	rules: {
 		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+		'unused-imports/no-unused-imports-ts': 'error',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'react/display-name': [0, { ignoreTranspilerName: true }],
-		'autofix/no-unused-vars': 'error',
+		'no-empty-pattern': 'warn',
 	},
 };
