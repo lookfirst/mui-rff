0279aae69652923394b96a101a26178b3e44f347
Santiago Garcia <santiago.garcia@posteo.de>
2021-10-11 18:30:00 +0200

    Duplicate tests for deprecated pickers

---
+++ test/KeyboardDatePicker.test.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import { Form } from 'react-final-form';
+
+import 'date-fns';
+
+import { KeyboardDatePicker } from '../src';
+import { act, customRender } from './TestUtils';
+
+interface ComponentProps {
+	initialValues: FormData;
+	validator?: any;
+}
+
+interface FormData {
+	date: Date;
+}
+
+describe('KeyboardDatePicker', () => {
+	const defaultDateValue = '2019-10-18';
+	const defaultDateString = `${defaultDateValue}T00:00:00`;
+
+	const initialValues: FormData = {
+		date: new Date(defaultDateString),
+	};
+
+	function KeyboardDatePickerComponent({ initialValues, validator }: ComponentProps) {
+		const onSubmit = (values: FormData) => {
+			console.log(values);
+		};
+
+		const validate = async (values: FormData) => {
+			if (validator) {
+				return validator(values);
+			}
+		};
+
+		return (
+			<Form
+				onSubmit={onSubmit}
+				initialValues={initialValues}
+				validate={validate}
+				render={({ handleSubmit }) => (
+					<form onSubmit={handleSubmit} noValidate>
+						<KeyboardDatePicker label="Test" name="date" required={true} inputFormat="yyyy-MM-dd" />
+					</form>
+				)}
+			/>
+		);
+	}
+
+	it('renders without errors', async () => {
+		await act(async () => {
+			const rendered = customRender(<KeyboardDatePickerComponent initialValues={initialValues} />);
+			expect(rendered).toMatchSnapshot();
+		});
+	});
+
+	it('renders the value with default data', async () => {
+		const rendered = customRender(<KeyboardDatePickerComponent initialValues={initialValues} />);
+		const date = (await rendered.findByDisplayValue(defaultDateValue)) as HTMLInputElement;
+		expect(date.value).toBe(defaultDateValue);
+	});
+
+	it('has the Test label', async () => {
+		await act(async () => {
+			const rendered = customRender(<KeyboardDatePickerComponent initialValues={initialValues} />);
+			const elem = rendered.getByText('Test') as HTMLLegendElement;
+			expect(elem.tagName).toBe('LABEL');
+		});
+	});
+
+	it('has the required *', async () => {
+		await act(async () => {
+			const rendered = customRender(<KeyboardDatePickerComponent initialValues={initialValues} />);
+			const elem = rendered.getByText('*') as HTMLSpanElement;
+			expect(elem.tagName).toBe('SPAN');
+			expect(elem.innerHTML).toBe(' *');
+		});
+	});
+});
---
+++ test/KeyboardDateTimePicker.test.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import { Form } from 'react-final-form';
+
+import 'date-fns';
+
+import { KeyboardDateTimePicker } from '../src';
+import { act, customRender } from './TestUtils';
+
+interface ComponentProps {
+	initialValues: FormData;
+	validator?: any;
+}
+
+interface FormData {
+	date: Date;
+}
+
+describe('KeyboardDateTimePicker', () => {
+	const defaultDateTimeValue = '2019-10-18 12:00 AM';
+
+	const initialValues: FormData = {
+		date: new Date(defaultDateTimeValue),
+	};
+
+	function KeyboardDateTimePickerComponent({ initialValues, validator }: ComponentProps) {
+		const onSubmit = (values: FormData) => {
+			console.log(values);
+		};
+
+		const validate = async (values: FormData) => {
+			if (validator) {
+				return validator(values);
+			}
+		};
+
+		return (
+			<Form
+				onSubmit={onSubmit}
+				initialValues={initialValues}
+				validate={validate}
+				render={({ handleSubmit }) => (
+					<form onSubmit={handleSubmit} noValidate>
+						<KeyboardDateTimePicker
+							label="Test"
+							name="date"
+							required={true}
+							inputFormat="yyyy-MM-dd h:mm a"
+						/>
+					</form>
+				)}
+			/>
+		);
+	}
+
+	it('renders without errors', async () => {
+		await act(async () => {
+			const rendered = customRender(<KeyboardDateTimePickerComponent initialValues={initialValues} />);
+			expect(rendered).toMatchSnapshot();
+		});
+	});
+
+	it('renders the value with default data', async () => {
+		const rendered = customRender(<KeyboardDateTimePickerComponent initialValues={initialValues} />);
+		const date = (await rendered.findByDisplayValue(defaultDateTimeValue)) as HTMLInputElement;
+		expect(date.value).toBe(defaultDateTimeValue);
+	});
+
+	it('has the Test label', async () => {
+		await act(async () => {
+			const rendered = customRender(<KeyboardDateTimePickerComponent initialValues={initialValues} />);
+			const elem = rendered.getByText('Test') as HTMLLegendElement;
+			expect(elem.tagName).toBe('LABEL');
+		});
+	});
+
+	it('has the required *', async () => {
+		await act(async () => {
+			const rendered = customRender(<KeyboardDateTimePickerComponent initialValues={initialValues} />);
+			const elem = rendered.getByText('*') as HTMLSpanElement;
+			expect(elem.tagName).toBe('SPAN');
+			expect(elem.innerHTML).toBe(' *');
+		});
+	});
+});
