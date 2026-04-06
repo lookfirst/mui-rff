85cf79c2ed20df72fe1ff601b55ee198881e11ca
Public Profile <latchkey@gmail.com>
2026-04-06 11:08:27 -0700

    chore: update deps

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'node:path';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
